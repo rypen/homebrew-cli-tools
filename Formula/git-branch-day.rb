fb1a3ff8bc47cf364cd847dbed7aca09b4eda828
Ryan Willis <code@ryanwillis.com>
2026-01-14 17:22:42 -0700

    upgrade git-branch-day to v0.2.0

--- Formula/git-branch-day.rb
+++ Formula/git-branch-day.rb
@@ -1,9 +1,14 @@
 class GitBranchDay < Formula
   desc "Git extension to spread commits over a full day"
   homepage "https://github.com/rypen/git-branch-day"
-  url "https://github.com/rypen/git-branch-day/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "a8a4985bfc5098f8ab25b050f0637aa7771b20a61cc06c7fab2e8f0032cf7657"
+  url "https://github.com/rypen/git-branch-day/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "6c255437d10e9e87a25eb7c0ecede0c7976b4d3b401f553ea01cdd1534579051"
   head "https://github.com/rypen/git-branch-day.git"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
 
   depends_on "go" => :build
   depends_on "git"
