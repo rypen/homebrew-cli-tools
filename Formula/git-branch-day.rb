8a4e8bb9a0aa525d82853b24539b1758d238a8b6
Ryan Willis <code@ryanwillis.com>
2026-01-14 17:03:20 -0700

    install from source

--- Formula/git-branch-day.rb
+++ Formula/git-branch-day.rb
@@ -1,6 +1,8 @@
 class GitBranchDay < Formula
   desc "Git extension to spread commits over a full day"
   homepage "https://github.com/rypen/git-branch-day"
+  url "https://github.com/rypen/git-branch-day/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "a8a4985bfc5098f8ab25b050f0637aa7771b20a61cc06c7fab2e8f0032cf7657"
   head "https://github.com/rypen/git-branch-day.git"
 
   depends_on "go" => :build
